0e41231caf37f4319a8ed7664215fbcfb1df9793
stevejboyle <sboyle@paloaltonetworks.com>
2022-12-01 11:01:52 -0500

    Part 2 added

--- day1/test2.py
+++ day1/test2.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 with open("input.txt", "r") as input_line:
-    max_val = 0
-    cur_val = 0
+    grand_total = 0
+    elf_list = []
+    i = 0
 
+    elf_list.append(0)
     for line in input_line:
         if line.strip() == "":
-            if cur_val > max_val:
-                max_val = cur_val
-            cur_val = 0
+            i += 1
+            elf_list.append(0)
         else:
-            cur_val += int(line)
-    print(max_val)
+            elf_list[i] += int(line)
+    elf_list.sort()
+    grand_total = sum(elf_list[-3:])
+    print(grand_total)
