6031987611ff71176e99fb63ef1be25066ad5e6d
stevejboyle <sboyle@paloaltonetworks.com>
2022-12-02 14:21:37 -0500

    Day 2 finished

--- day2/test2.py
+++ day2/test2.py
@@ -7,17 +7,28 @@ with open("input.txt", "r") as input_line:
         score = 0
         a, b = line.split(' ', 1)
         b = b.rstrip()
-        if a == 'A':
-            score += 1
-        elif a == 'B':
-            score += 2
-        elif a == 'C':
+        if b == 'X':
+            if a == 'A':
+                score += 3
+            elif a == 'B':
+                score += 1
+            elif a == 'C':
+                score += 2
+        elif b == 'Y':
             score += 3
-
-        if b == 'Y':
-            score += 3
+            if a == 'A':
+                score += 1
+            elif a == 'B':
+                score += 2
+            elif a == 'C':
+                score += 3
         elif b == 'Z':
             score += 6
-
+            if a == 'A':
+                score += 2
+            elif a == 'B':
+                score += 3
+            elif a == 'C':
+                score += 1
         total += score
     print(total)
